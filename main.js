ce50e3f4d2f043b4f317de99a69ef88dda774f95
Moi <moira.lasserre@gmail.com>
2020-04-05 20:25:30 -0300

    first scripts

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
+require("dotenv").config();
 const Discord = require('discord.js');
+const fs = require("fs")
 const client = new Discord.Client();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+fs.readdir("./events/", (err, files) => {
+  files.forEach(file => {
+    const eventHandler = require(`./events/${file}`);
+    const eventName = file.split(".")[0]
+    client.on(eventName, (...args) => eventHandler(client, ...args))
+  })
+})
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
-});
 
-client.login('token');
+client.login(process.env.BOT_TOKEN);
